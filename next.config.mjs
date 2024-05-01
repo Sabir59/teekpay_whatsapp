20441cc3f77729d86683501a5b2193efc482060e
Sabir Nawaz <sabirnawaz59@gmail.com>
2024-05-01 13:49:41 -0700

    font issue resolved

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
   images: {
     domains: ['gravatar.com'], // Add 'gravatar.com' to the list of allowed domains
   },
